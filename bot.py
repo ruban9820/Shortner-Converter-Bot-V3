304c7115ee05f728729d7f9da42259d298561c53
ruban9820 <130816373+ruban9820@users.noreply.github.com>
2024-02-09 21:53:37 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,61 @@
 
+import asyncio
+import datetime
+import logging
+import logging.config
+import sys
+from pyrogram import *
+from pyrogram.errors.exceptions.not_acceptable_406 import *
+from config import *
+from database import *
+from database.users import *
+from helpers import *
+from pyshorteners import *
+logging.config.fileConfig('logging.conf')
+logging.getLogger().setLevel(logging.INFO)
+import os
+import pyrogram
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+import logging
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+
+if __name__ == "__main__" :
+
+    plugins = dict(
+        root="plugins"
+    )
+    dkbotz = Client(
+        "Mdisk-Pro",
+        bot_token=BOT_TOKEN,
+        api_id=API_ID,
+        api_hash=API_HASH,
+        plugins=plugins
+    )
+    
+    async def start(self):
+        me = await self.get_me()
+        self.owner = await self.get_users(int(OWNER_ID))
+        self.username = f'@{me.username}'
+        temp.BOT_USERNAME = me.username
+        temp.FIRST_NAME = me.first_name
+        if not await db.get_bot_stats():
+            await db.create_stats()
+        banned_users = await filter_users({"banned": True})
+        async for user in banned_users:
+            temp.BANNED_USERS.append(user["user_id"])
+        logging.info(LOG_STR)
+        await broadcast_admins(self, '** Bot started successfully **\n\nBot By @DKBOTZ')
+        logging.info('Bot started')
+
+
+    dkbotz.run()
+
+
+
+    async def stop(self, *args):
+        await super().stop()
+        logging.info("Bot stopped. Bye.")
+
